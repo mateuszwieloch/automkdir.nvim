6ea08f4bdcb179ee479e846a8e04b2a9a2011524
OndraK <2472004+ondrejkajinek@users.noreply.github.com>
2025-03-11 21:51:33 +0100

    fix: apply CR changes

--- plugin/automkdir.lua
+++ plugin/automkdir.lua
@@ -1,8 +1,11 @@
 local automkdirGroup = vim.api.nvim_create_augroup("automkdirGroup", { clear = true })
 
+--- Returns an array of paths to `path`'s parent directories. Starts from top-most directory (filesystem root) and continues to `path`'s parent.
+---@param path string Path to a file. Doesn't have to be normalized.
+---@return string[]
 local function get_parent_directories(path)
   path = vim.fs.normalize(path)
-  local parents = {path}
+  local parents = {}
   local parent = vim.fs.dirname(path)
 
   while parent ~= path do
@@ -14,15 +17,13 @@ local function get_parent_directories(path)
 end
 
 vim.api.nvim_create_autocmd('BufWritePre', {
+  -- Callback function gets a table that contains match key, which maps to `<amatch>` (a full filepath).
   callback = function(t)
-    -- Function gets a table that contains match key, which maps to `<amatch>` (a full filepath).
-    local target_dir = vim.fs.dirname(t.match)
-    -- Attempt to recursively mkdir. If dir already exists, it returns nil.
-    -- Use 755 permissions, which means rwxr.xr.x
-    for _, dirname in pairs(get_parent_directories(target_dir)) do
-      if not vim.loop.fs_stat(dirname) then
-        vim.loop.fs_mkdir(dirname, tonumber("0755", 8))
-      end
+    -- Attempt to mkdir all parent directories of filepath: from filesystem root directory all the way to filepath's parent dir.
+    for _, dirname in ipairs(get_parent_directories(t.match)) do
+      -- Use 755 permissions, which means rwxr.xr.x
+      -- If dir already exists, fs_mkdir returns nil and does nothing.
+      vim.uv.fs_mkdir(dirname, tonumber("0755", 8))
     end
   end,
   group = automkdirGroup
